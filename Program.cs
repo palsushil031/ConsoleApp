7b402c805c4cbff6c56392bc345993fc8a03dabf
Sushil Pal <palsushil031@gmail.com>
2023-06-06 14:49:12 +0530

    consoleApp

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a = 10;
+int b = 20;
+Console.WriteLine($"value of a = {a} value of b = {b}");
+a = a * b;   // 200
+b = a / b;   // 10
+a = a / b;   // 20
+
+Console.WriteLine($"value of a = {a} value of b = {b}");
